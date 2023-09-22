4f4e979cbde0e212b2325aba7aeb9de5eda6b301
Anton <l1tovkin@mail.ru>
2023-09-22 15:59:11 +0300

    lint created

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,7 @@
 import readlineSync from 'readline-sync';
 
 export default function askName() {
-    const name = readlineSync.question('May I have your name? ');
-    const answer = `Hello, ${name}`;
-    console.log(answer)
+  const name = readlineSync.question('May I have your name? ');
+  const answer = `Hello, ${name}`;
+  console.log(answer);
 }
